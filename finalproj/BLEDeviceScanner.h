5698d0440da8a687376dfa30cfc911c14a117033
Vincent Zhang <73851560+bootsareme@users.noreply.github.com>
2026-04-19 21:23:28 -0400

    Refactor BLEDeviceScanner to include device statistics

--- finalproj/BLEDeviceScanner.h
+++ finalproj/BLEDeviceScanner.h
@@ -7,27 +7,44 @@
 #include <vector>
 #include <string>
 
-enum class DeviceType {
-    MOBILE,
-    LAPTOP,
-    WEARABLE,
-    UNKNOWN
-};
+#define SCAN_DURATION 3 // in seconds, how long to scan for ads
+#define SCAN_DUTY_CYCLE 99 // % of scanning interval taken by scanning window
 
-struct PersonalDevice {
-    std::string mac;
-    std::string name;
-    int rssi;
-    std::string manufacturer; // e.g., "Apple", "Microsoft", "Unknown (Randomized)"
-    DeviceType type;
+struct Summary {
+    unsigned short mobiles;
+    unsigned short laptops;
+    unsigned short wearables;
+    unsigned short unknowns;
+
+    unsigned short apples;
+    unsigned short googles;
+    unsigned short microsofts;
+    unsigned short samsungs;
+    unsigned short others;
 };
 
 class BLEDeviceScanner
 {
+    enum class DeviceType {
+        MOBILE,
+        LAPTOP,
+        WEARABLE,
+        UNKNOWN
+    };
+
+    enum class Manufacturer {
+        APPLE,
+        MICROSOFT,
+        GOOGLE,
+        SAMSUNG,
+        OTHER
+    };
+
 public:
     void begin();
-    std::vector<PersonalDevice> scan(uint32_t duration_secs);
+    Summary getStats(uint32_t duration_secs);
+
 private:
     BLEScan *pBLEScan;
-    static bool isLikelyPersonalDevice(BLEAdvertisedDevice *device, std::string &out_manufacturer, DeviceType &out_type);
+    static bool isLikelyPersonalDevice(BLEAdvertisedDevice *device, Manufacturer &out_manu, DeviceType &out_type);
 };
